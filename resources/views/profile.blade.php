05af30a44ffd4fd1c9dd338189558e0195b2bfd8
Reight <clement_praud@yahoo.com>
2024-04-04 18:09:35 +0200

    Dev: Front/CSS user profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,39 +3,42 @@
     <head>
         <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
     </head>
-    <body>    
+    <body>
         <div class="sign">
+            <div id="title"> 
+                <img src="../img/user_image.jpg" />Votre profil :
+            </div>
             <form action="/auth/register" method="post" >
             @csrf
             <div class="grill">
                 <label for="name">Pseudo: </label>
-                <input type="text" name="name" class="name" required/>
+                <input type="text" name="name" value="{{ Auth::user()->name }}" required/>
                 <label for="email">Email: </label>
-                <input type="email" name="email" class="email" required/>
+                <input type="email" name="email" value="{{Auth::user()->email}}" required/>
                 <label for="first_name">Prenom: </label>
-                <input type="first_name" name="first_name" class="first_name"/>
+                <input type="first_name" name="first_name" value="{{Auth::user()->first_name}}"/>
                 <label for="last_name">Nom: </label>
-                <input type="text" name="last_name" class="last_name" />
+                <input type="text" name="last_name" value="{{Auth::user()->last_name}}"/>
                 <label for="birth">Date de naissance: </label>
-                <input type="date" name="birth" class="birth"/>
+                <input type="date" name="birth" value="{{Auth::user()->birth}}"/>
                 <legend>Genre: </legend>
                 <div id=genre>
                     <label for="gender">Homme </label>
-                    <input type="radio" name="gender" value="male" class="gender"/>
+                    <input type="radio" name="gender" value="male"/>
                     <label for="gender">Femme </label>
-                    <input type="radio" name="gender" value="female" class="gender"/>
+                    <input type="radio" name="gender" value="female"/>
                     <label for="gender">Autre </label>
-                    <input type="radio" name="gender" value="other" class="gender"/>
+                    <input type="radio" name="gender" value="other"/>
                 </div>
-                <label for="profession">Metier: </label>
-                <input type="text" name="profession" class="profession"/>
+                <label for="job">Metier: </label>
+                <input type="text" name="job" value="{{Auth::user()->job}}"/>
                 <label for="password">Mot de passe: </label>
-                <input type="password" name="password" class="password" required/>
+                <input type="password" name="password" required/>
                 <label for="password_confirmation">Confirmation du mot de passe: </label>
-                <input type="password" name="password_confirmation" class="password" required/>
+                <input type="password" name="password_confirmation" required/>
             </div>
             <div class="button">
-                <input type="submit" value="Mettre a jour le profil"/>
+                <input type="submit" value="Modifier le profil"/>
             </div>
             </form>
         </div>
