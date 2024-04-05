1e9f5260756c9fb033f39c4909e7b542f0dccfd2
Raphaël Fromentin <42907886+Iltotore@users.noreply.github.com>
2024-04-05 15:04:27 +0200

    feat: Add cart page (#36)
    The page still needs to be stylished. Maybe @Reight123 can do it in a
    future PR?
    
    Related to #33

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
@@ -25,9 +26,9 @@ class Product extends Model
         'category_id'
     ];
 
-    public function category(): HasOne
+    public function category(): BelongsTo
     {
-        return $this->hasOne(Category::class);
+        return $this->belongsTo(Category::class);
     }
 
     public function buyers(): BelongsToMany {
---
+++ public/js/cart.js
@@ -0,0 +1,80 @@
+async function addItem(id, token) {
+    const result = await sendJSON("/cart/add", {
+        product: id,
+        amount: 1,
+        _token: token
+    })
+
+    const itemDiv = document.getElementById("item#" + id);
+    const itemAmount = itemDiv.getElementsByClassName("amount")[0]
+    const itemAdd = itemDiv.getElementsByClassName("add")[0]
+
+    if(result.status === 200) {
+        const jsonResponse = JSON.parse(result.responseText)
+        switch (jsonResponse.state) {
+            case "ok":
+                itemAmount.innerHTML = "x" + jsonResponse.amount
+                itemAdd.disabled = false
+                break
+
+            case "full":
+                itemAmount.innerHTML = "x" + jsonResponse.amount
+                itemAdd.disabled = true
+                break
+        }
+    }
+}
+
+async function removeItem(id, token) {
+    const result = await sendJSON("/cart/remove", {
+        product: id,
+        amount: 1,
+        _token: token
+    })
+
+    const itemDiv = document.getElementById("item#" + id);
+    const itemAmount = itemDiv.getElementsByClassName("amount")[0]
+    const itemAdd = itemDiv.getElementsByClassName("add")[0]
+
+    if(result.status === 200) {
+        const jsonResponse = JSON.parse(result.responseText)
+        switch (jsonResponse.state) {
+            case "ok":
+                itemAmount.innerHTML = "x" + jsonResponse.amount
+                itemAdd.disabled = false
+                break
+
+            case "full":
+                itemAmount.innerHTML = "x" + jsonResponse.amount
+                itemAdd.disabled = true
+                break
+
+            case "doesNotExist":
+            case "delete":
+                itemDiv.remove()
+        }
+    }
+}
+
+async function deleteItem(id, token) {
+    const result = await sendJSON("/cart/delete", {product: id, _token: token})
+    if(result.status === 200) {
+        document.getElementById("item#" + id).remove()
+    }
+}
+
+async function buyItems(token) {
+    const result = await sendJSON("/cart/buy", {_token: token})
+
+    if(result.status === 200) {
+        document.getElementById("items").innerHTML = ""
+    }
+}
+
+async function clearItems(token) {
+    const result = await sendJSON("/cart/clear", {_token: token})
+
+    if(result.status === 200) {
+        document.getElementById("items").innerHTML = ""
+    }
+}
