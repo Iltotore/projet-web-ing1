da607673116e35a7f37960a58b6770c3babfd5a2
Raphaël Fromentin <rafbodaha@gmail.com>
2024-04-08 08:51:13 +0200

    feat: Add catalog test

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,24 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Product;
-use http\Exception\RuntimeException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
-class CatalogController extends Controller
-{
+class CatalogController extends Controller {
 
     /**
      * Get products of a category.
      *
      * @return JsonResponse
      */
-    public function getProducts(Request $request): JsonResponse
-    {
+    public function getProducts(Request $request): JsonResponse {
         $args = $request->validate([
-            "category" =>  ["required", "integer", "exists:categories,id"]
+            "category" => ["required", "integer", "exists:categories,id"]
         ]);
 
         $products = Product::where("category_id", "=", $args["category"])->get();
