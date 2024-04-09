824c6823cb3b4548f09ba99785636f619dcc0c12
Raphaël Fromentin <rafbodaha@gmail.com>
2024-04-09 17:00:06 +0200

    feat: Add custom confirmation mail

--- resources/views/mail/register.blade.php
+++
@@ -1,8 +0,0 @@
-@php use Illuminate\Support\Facades\URL; @endphp
-<body>
-<h1>Inscription</h1>
-<label>
-    Bonjour {{ $user->first_name }} !
-    Inscrivez-vous ici: {{ URL::to("/confirm?token=" . $user->registration_token) }}
-</label>
-</body>
