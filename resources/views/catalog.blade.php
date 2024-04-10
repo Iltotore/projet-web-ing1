62e16d0d5e2ca58f92442cc99950bc8c5193bb3d
Reight <clement_praud@yahoo.com>
2024-04-10 22:34:19 +0200

    Dev: front+css

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,21 +1,30 @@
 <!DOCTYPE html>
-<body onload="loadProducts('{{csrf_token()}}')">
-<div id="container"></div>
-<div id="details" hidden>
-    <img id="icon" src="{{ asset("img/placeholder.png") }}" alt="icon"/>
-    <h1 id="name"></h1>
-    <label>Prix:</label>
-    <label id="price"></label>
-    <label>En stock:</label>
-    <label id="available"></label>
-    <label>Dans le panier:</label>
-    <label id="in_cart"></label>
-    <p id="description"></p>
-    <div id="actions">
-        <input name="amount" type="number" value="1">
-        <button id="add" onclick="addItem()">Ajouter au panier</button>
-        <button id="remove" onclick="removeItem()">Supprimer du panier</button>
-        <button id="close" onclick="closeProduct()">Fermer</button>
-    </div>
-</div>
-</body>
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/catalog.css') }}">
+    </head>
+    <body onload="loadProducts('{{csrf_token()}}')">
+        <div class="box">
+            <div id="container">
+                <div id="title">Catalogue</div>
+            </div>
+            <div id="details" hidden>
+                <img id="icon" src="{{ asset("img/placeholder.png") }}" alt="icon"/>
+                <h1 id="name"></h1>
+                <label>Prix:</label>
+                <label id="price"></label>
+                <label>En stock:</label>
+                <label id="available"></label>
+                <label>Dans le panier:</label>
+                <label id="in_cart"></label>
+                <p id="description"></p>
+                <div id="actions">
+                    <input name="amount" type="number" value="1">
+                    <button id="add" onclick="addItem()">Ajouter au panier</button>
+                    <button id="remove" onclick="removeItem()">Supprimer du panier</button>
+                    <button id="close" onclick="closeProduct()">Fermer</button>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
