1c472f1c813b69ee04e1f0297bffb18ffc9dbaf1
Reight <clement_praud@yahoo.com>
2024-04-13 15:12:48 +0200

    Dev: Home PHP/CSS + Clean Database

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,105 @@
-<h1>Minceraft</h1>
-<p>Bienvenue ! Hmmm huurrrr hrm huh.</p>
+<!DOCTYPE html>
+<html>
+    <body>
+        <div class="box">
+            <div class="box2">
+                <div class="subsign">
+                    <div id="title">BIENVENUE</div>
+                    <hr/>
+                    <div>Créé par La Ligue des Villageois et Marchands Humains (LVMH), HurrShop est le site de référence pour l'achat rapide d'objets disponibles dans les villages. Retrouvez vos produits favoris issus de l'Overworld, du Nether ou de l'Ender à moindre prix.
+                    </div>
+                </div>
+                <div class="subsign">
+                    <div id="title">Votre profil :</div>
+                    <hr/>
+                    <div class="button">
+                        <button onclick="window.location.href = '/profile'">Votre profil</button>
+                        <button onclick="window.location.href = '/login'">Se connecter</button>
+                    </div>
+                </div>
+            </div>
+            <div class="sign">
+                <div id="title">CATALOGUE :</div>
+                <hr/>
+                <div class="slider">
+                    <div class="slider-viewport">
+                        <div id="img1" class="slider-content">
+                            <a href="/catalog?category=10">
+                                <img src="../category/Alambic.webp">
+                            </a>
+                        </div>
+                        <div id="img2" class="slider-content">
+                            <a href="/catalog?category=11">
+                                <img src="../category/Chaudron_plein.webp">
+                            </a>
+                        </div>
+                        <div id="img3" class="slider-content">
+                            <a href="/catalog?category=5">
+                                <img src="../category/Composteur_rempli.webp">
+                            </a>
+                        </div>
+                        <div id="img4" class="slider-content">
+                            <a href="/catalog?category=3">
+                                <img src="../category/Fumoir_allume.webp">
+                            </a>
+                        </div>
+                        <div id="img5" class="slider-content">
+                            <a href="/catalog?category=0">
+                                <img src="../category/Haut_fourneau_allume.webp">
+                            </a>
+                        </div>
+                        <div id="img6" class="slider-content">
+                            <a href="/catalog?category=1">
+                                <img src="../category/Metier_a_tisser.webp">
+                            </a>
+                        </div>
+                        <div id="img8" class="slider-content">
+                        <a href="/catalog?category=2">
+                            <img src="../category/Pupitre.webp">
+                            </a>
+                        </div>
+                        <div id="img9" class="slider-content">
+                        <a href="/catalog?category=6">
+                            <img src="../category/Table_d_archerie.webp">
+                            </a>
+                        </div>
+                        <div id="img10" class="slider-content">
+                        <a href="/catalog?category=4">
+                            <img src="../category/Table_de_cartographie.webp">
+                            </a>
+                        </div>
+                        <div id="img11" class="slider-content">
+                            <a href="/catalog?category=7">
+                                <img src="../category/Table_de_forgeron.webp">
+                            </a>
+                        </div>
+                        <div id="img12" class="slider-content">
+                            <a href="/catalog?category=8">
+                                <img src="../category/Tailleur_de_pierre.webp">
+                            </a>
+                        </div>
+                        <div id="img13" class="slider-content">
+                            <a href="/catalog?category=9">
+                                <img src="../category/Tonneau_ouvert.webp">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="slider-nav">
+                        <a href="#img1"></a>
+                        <a href="#img2"></a>
+                        <a href="#img3"></a>
+                        <a href="#img4"></a>
+                        <a href="#img5"></a>
+                        <a href="#img6"></a>
+                        <a href="#img8"></a>
+                        <a href="#img9"></a>
+                        <a href="#img10"></a>
+                        <a href="#img11"></a>
+                        <a href="#img12"></a>
+                        <a href="#img13"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
