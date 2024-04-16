e9bcf33d9737936396a107eb4d4f9067760c91a5
Reight <clement_praud@yahoo.com>
2024-04-16 14:17:40 +0200

    Dev: CSS on minor page

--- resources/views/error.blade.php
+++ resources/views/error.blade.php
@@ -1,2 +1,14 @@
-<h1>Erreur !</h1>
-<p>Hurr. Huh. Pin ! Puh ! Puh ! <i>Pwuh !</i></p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/about.css') }}">
+    </head>
+    <body>
+        <div class="box">
+            <div class="sign">
+                <h1>Erreur !</h1>
+                <p>Hurr. Huh. Pin ! Puh ! Puh ! <i>Pwuh !</i></p>
+            </div>
+        </div>
+    </body>
+</html>
--- resources/views/password-forgot.blade.php
+++ resources/views/password-forgot.blade.php
@@ -1,13 +1,21 @@
+<!DOCTYPE html>
 <html>
-<body>
-<h1>Réinitialiser son mot de passe</h1>
-<form method="post" action="/auth/password-request">
-    @csrf
-    <input type="email" name="email" class="@error('email') invalid @enderror">
-    @error("email")
-        <li class="error">{{ $message }}</li>
-    @enderror
-    <input type="submit" value="Envoyer">
-</form>
-</body>
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/about.css') }}">
+    </head>
+    <body>
+        <div class="box">
+            <div class="sign">
+                <h1>Réinitialiser son mot de passe</h1>
+                <form method="post" action="/auth/password-request">
+                    @csrf
+                    <input type="email" name="email" class="@error('email') invalid @enderror">
+                    @error("email")
+                        <li class="error">{{ $message }}</li>
+                    @enderror
+                    <input type="submit" value="Envoyer">
+                </form>
+            </div>
+        </div>
+    </body>
 </html>
