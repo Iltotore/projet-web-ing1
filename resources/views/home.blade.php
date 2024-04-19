7e03ebb6847c088b6f91c391c45c30b9aa131608
gaetan <gaetan.g237@gmail.com>
2024-04-19 11:56:58 +0200

    fix: category list in home.php wasn't depending of category list in database ??? @Reight123 what was that crap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,80 +23,18 @@
                 <hr/>
                 <div class="slider">
                     <div class="slider-viewport">
-                        <div id="img1" class="slider-content">
-                            <a href="/catalog?category=10">
-                                <img src="../category/Alambic.webp">
-                            </a>
-                        </div>
-                        <div id="img2" class="slider-content">
-                            <a href="/catalog?category=11">
-                                <img src="../category/Chaudron_plein.webp">
-                            </a>
-                        </div>
-                        <div id="img3" class="slider-content">
-                            <a href="/catalog?category=5">
-                                <img src="../category/Composteur_rempli.webp">
-                            </a>
-                        </div>
-                        <div id="img4" class="slider-content">
-                            <a href="/catalog?category=3">
-                                <img src="../category/Fumoir_allume.webp">
-                            </a>
-                        </div>
-                        <div id="img5" class="slider-content">
-                            <a href="/catalog?category=0">
-                                <img src="../category/Haut_fourneau_allume.webp">
-                            </a>
-                        </div>
-                        <div id="img6" class="slider-content">
-                            <a href="/catalog?category=1">
-                                <img src="../category/Metier_a_tisser.webp">
-                            </a>
-                        </div>
-                        <div id="img8" class="slider-content">
-                        <a href="/catalog?category=2">
-                            <img src="../category/Pupitre.webp">
-                            </a>
-                        </div>
-                        <div id="img9" class="slider-content">
-                        <a href="/catalog?category=6">
-                            <img src="../category/Table_d_archerie.webp">
-                            </a>
-                        </div>
-                        <div id="img10" class="slider-content">
-                        <a href="/catalog?category=4">
-                            <img src="../category/Table_de_cartographie.webp">
-                            </a>
-                        </div>
-                        <div id="img11" class="slider-content">
-                            <a href="/catalog?category=7">
-                                <img src="../category/Table_de_forgeron.webp">
-                            </a>
-                        </div>
-                        <div id="img12" class="slider-content">
-                            <a href="/catalog?category=8">
-                                <img src="../category/Tailleur_de_pierre.webp">
-                            </a>
-                        </div>
-                        <div id="img13" class="slider-content">
-                            <a href="/catalog?category=9">
-                                <img src="../category/Tonneau_ouvert.webp">
-                            </a>
-                        </div>
+                        @foreach(\App\Models\Category::all()->sortBy('name') as $category)
+                           <div id="img_{{$category->name}}" class="slider-content">
+                                <a href="/catalog?category={{$category->id}}">
+                                    <img src="{{ asset("category/" . $category['icon']) }}" alt="{{$category->name}} category image">
+                                </a>
+                            </div>
+                        @endforeach
                     </div>
                     <div class="slider-nav">
-                        <a href="#img1"></a>
-                        <a href="#img2"></a>
-                        <a href="#img3"></a>
-                        <a href="#img4"></a>
-                        <a href="#img5"></a>
-                        <a href="#img6"></a>
-                        <a href="#img8"></a>
-                        <a href="#img9"></a>
-                        <a href="#img10"></a>
-                        <a href="#img11"></a>
-                        <a href="#img12"></a>
-                        <a href="#img13"></a>
+                        @foreach(\App\Models\Category::all()->sortBy('name') as $category)
+                            <a href="#img_{{$category->name}}"></a>
+                        @endforeach
                     </div>
                 </div>
             </div>
