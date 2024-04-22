8babe16544be9df74cd460a586a5aee212cbc7d2
Raphaël Fromentin <rafbodaha@gmail.com>
2024-04-22 13:59:58 +0200

    feat: Stylize password reset

--- resources/views/password-reset.blade.php
+++ resources/views/password-reset.blade.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="{{ asset('css/about.css') }}">
-    </head>
-    <body>
-        <div class="box">
-            <div class="sign">
-                <form method="post" action="/auth/reset-password">
-                    @csrf
-                    <input type="hidden" name="token" value="{{ $token }}">
-                    <input type="email" name="email" class="@error('email') invalid @enderror">
-                    @error("email")
-                        <li class="error">{{ $message }}</li>
-                    @enderror
-                    <input type="password" name="password" class="@error('password') invalid @enderror">
-                    @error("password")
-                        <li class="error">{{ $message }}</li>
-                    @enderror
-                    <input type="password" name="password_confirmation" class="@error('password') invalid @enderror">
-                    @error("password")
-                        <li class="error">{{ $message }}</li>
-                    @enderror
-                    <input type="submit" value="Réinitialiser mon mot de passe">
-                </form>
+<body>
+<div class="box">
+    <div class="sign">
+        <form method="post" action="/auth/reset-password">
+            @csrf
+            <input type="hidden" name="token" value="{{ $token }}">
+            <div class="grill">
+                <label for="email">Email: </label>
+                <input type="email" name="email" class="@error('email') invalid @enderror">
+                @error("email")
+                <li class="error">{{ $message }}</li>
+                @enderror
+                <label for="password">Mot de passe: </label>
+                <input type="password" name="password" class="@error('password') invalid @enderror">
+                @error("password")
+                <li class="error">{{ $message }}</li>
+                @enderror
+                <label for="password_confirmation">Confirmer le mot de passe: </label>
+                <input type="password" name="password_confirmation" class="@error('password') invalid @enderror">
+                @error("password")
+                <li class="error">{{ $message }}</li>
+                @enderror
             </div>
-        </div>
-    </body>
+            <input class="button" type="submit" value="Réinitialiser mon mot de passe">
+        </form>
+    </div>
+</div>
+</body>
 </html>
